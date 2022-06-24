bc6f3d539c4480505a32d0ff34d10595e3e5ce3d
ashikmeerankutty <ashik9591@gmail.com>
2022-06-24 12:17:51 +0530

    fix: end button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,23 @@
 import { Box } from "@twilio-paste/core/box";
 import { Text } from "@twilio-paste/core/text";
 import { CloseIcon } from "@twilio-paste/icons/esm/CloseIcon";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { closeStyles, containerStyles, infoStyles, titleStyles } from "./styles/Header.styles";
 import OwlIcon from "../assets/owl-icon-inverse.png";
-import { changeEngagementPhase } from "../store/actions/genericActions";
-import { EngagementPhase } from "../store/definitions";
+import { changeEngagementPhase, changeExpandedStatus } from "../store/actions/genericActions";
+import { AppState, EngagementPhase } from "../store/definitions";
 
 export const Header = ({ customTitle }: { customTitle?: string }) => {
     const dispatch = useDispatch();
+    const { conversation } = useSelector((state: AppState) => ({
+        conversation: state.chat.conversation
+    }));
     const endConversation = () => {
+        if (conversation?.state?.current !== "active") {
+            dispatch(changeExpandedStatus({ expanded: false }));
+            return;
+        }
         dispatch(changeEngagementPhase({ phase: EngagementPhase.EndMessage }));
     };
 
