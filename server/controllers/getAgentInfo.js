a80a168052898dc4ed8647bf5756e62909215694
ashikmeerankutty <ashik9591@gmail.com>
2022-07-11 15:29:02 +0530

    fix: add more agents

--- server/controllers/getAgentInfo.js
+++ server/controllers/getAgentInfo.js
@@ -1,13 +1,23 @@
 const sampleUsers = {
     "jenn-riannon": {
         name: "Jenn Riannon",
-        profilePic: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+        profilePic: "https://www.random-name-generator.com/images/faces/female-white/49.jpg",
         description:
             "I'm Jenn, and I'm dedicated to helping people find the home loan that comfortably fits their needs in the San Francisco area. As a President's Club member and a trusted partner to many people in this community, I'm here to guide you through every step of the home loan process. Call me today if you're ready to get started or would like more information.",
         role: "Credit Solutions Advisor",
         nmls: "1127543",
         phone: "202.918.2132",
         address: "2418 Simpson Street, Fiatt, Illinois"
+    },
+    "keith-cox": {
+        name: "Keith Cox",
+        profilePic: "https://www.random-name-generator.com/images/faces/male-white/03.jpg",
+        description:
+            "I'm Keith, and I'm dedicated to helping people find the home loan that comfortably fits their needs in the San Francisco area. As a President's Club member and a trusted partner to many people in this community, I'm here to guide you through every step of the home loan process. Call me today if you're ready to get started or would like more information.",
+        role: "Credit Solutions Advisor",
+        nmls: "1237543",
+        phone: "609.278.6558",
+        address: "4367 Dark Hollow Road, Trenton, New Jersey"
     }
 };
 
